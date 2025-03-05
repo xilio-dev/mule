6a223c44e60d5871f6ffff2c226d646eebdeee5e
xilio <termitetech@163.com>
2025-03-06 01:42:49 +0800

    增加账户设置：消息配置、黑名单列表管理

--- stackoak-site-vue/src/views/setting/black-list/index.vue
+++ stackoak-site-vue/src/views/setting/black-list/index.vue
@@ -3,9 +3,23 @@
 </script>
 
 <template>
-  <h1>和名单</h1>
+  <a-card style="width: 100%;" title="黑名单作者">
+    <a-card style="width: 160px" v-for="i in 1">
+      <a-flex vertical align="center" :gap="4">
+        <a-avatar :size="64" src="/avatar.jpeg"/>
+        <div>大小姐驾到</div>
+        <div style="font-size: 12px;color:rgb(138, 145, 159)">拉黑时间：2023-02-25</div>
+        <a-button type="dashed" style="width: 100%;">取消拉黑</a-button>
+      </a-flex>
+    </a-card>
+  </a-card>
 </template>
 
 <style scoped>
+
+:deep(.ant-card .ant-card-body) {
+  padding: 12px;
+  border-radius: 0 0 8px 8px;
+}
 
 </style>
--- stackoak-site-vue/src/views/setting/notification/index.vue
+++ stackoak-site-vue/src/views/setting/notification/index.vue
@@ -1,11 +1,78 @@
 <script setup lang="ts">
+import {ref} from "vue";
 
+const options = [...Array(25)].map((_, i) => ({value: (i + 10).toString(36) + (i + 1)}));
+const value2 = ref(['接收']);
 </script>
 
 <template>
-
+  <a-card>
+    <a-flex justify="space-between" align="center">
+      <a-flex vertical>
+        <div class="msg-type-name">评论消息</div>
+        <div class="desc-label">有人对我的内容进行评论时，我会收到消息提醒</div>
+      </a-flex>
+      <a-select
+          v-model:value="value2"
+          :options="options"
+          size="small"
+          placeholder="Please select"
+          style="width: 100px"
+      ></a-select>
+    </a-flex>
+    <a-divider/>
+    <a-flex justify="space-between" align="center">
+      <a-flex vertical>
+        <div class="msg-type-name">新增粉丝</div>
+        <div class="desc-label">有人关注我后会收到消息提醒</div>
+      </a-flex>
+      <a-select
+          v-model:value="value2"
+          :options="options"
+          size="small"
+          placeholder="Please select"
+          style="width: 100px"
+      ></a-select>
+    </a-flex>
+    <a-divider/>
+    <a-flex justify="space-between" align="center">
+      <a-flex vertical>
+        <div class="msg-type-name">赞和收藏</div>
+        <div class="desc-label">有人对我表达的内容表示赞同时，我会收到消息提醒</div>
+      </a-flex>
+      <a-select
+          v-model:value="value2"
+          :options="options"
+          size="small"
+          placeholder="Please select"
+          style="width: 100px"
+      ></a-select>
+    </a-flex>
+    <a-divider/>
+    <a-flex justify="space-between" align="center">
+      <a-flex vertical>
+        <div class="msg-type-name">陌生人私信</div>
+        <div class="desc-label">开启后陌生人可以私信我【所有人、我关注的人、互相关注的人、粉丝、关闭】</div>
+      </a-flex>
+      <a-select
+          v-model:value="value2"
+          :options="options"
+          size="small"
+          placeholder="Please select"
+          style="width: 100px"
+      ></a-select>
+    </a-flex>
+    <a-divider/>
+  </a-card>
 </template>
 
 <style scoped>
+.msg-type-name {
+  font-size: 15px;
+}
 
+.desc-label {
+  font-size: 13px;
+  color: #777888;
+}
 </style>
