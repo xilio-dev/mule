619fb28f8cd423c06197bbe897b14fec10c4f181
xilio <termitetech@163.com>
2025-03-29 11:37:29 +0800

    feat(文章详情): 新增文章二维码分享下载功能

--- stackoak-site-vue/src/views/post/Index.vue
+++ stackoak-site-vue/src/views/post/Index.vue
@@ -33,6 +33,7 @@ const parentCommentInputRef = ref()
 const needVisitPass = ref(false)
 const visitPass = ref('')
 const curCommentItem = ref()
+const qrcodeCanvasRef = ref();
 //打开登陆
 const openLoginModal = ref(false)
 const openCollectModel = ref(false)/*打开收藏夹模态框*/
@@ -302,7 +303,16 @@ const onOpenCollect = () => {
   openCollectModel.value = true
   loadCollects(1, collectPagination.pageSize)
 }
-
+//下载文章分享二维码
+const downloadChange = async () => {
+  const url = qrcodeCanvasRef.value.toDataURL();
+  const a = document.createElement('a');
+  a.download = `${articleInfo.value.id}.png`;
+  a.href = url;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
 </script>
 
 <template>
@@ -499,15 +509,26 @@ const onOpenCollect = () => {
         </svg>
       </template>
     </a-float-button>
-    <a-float-button :style="{marginTop: '25px'}">
-      <template #icon>
-        <svg viewBox="64 64 896 896" focusable="false" data-icon="share-alt" width="1em"
-             height="1em" fill="#8a919f" aria-hidden="true">
-          <path
-              d="M752 664c-28.5 0-54.8 10-75.4 26.7L469.4 540.8a160.68 160.68 0 000-57.6l207.2-149.9C697.2 350 723.5 360 752 360c66.2 0 120-53.8 120-120s-53.8-120-120-120-120 53.8-120 120c0 11.6 1.6 22.7 4.7 33.3L439.9 415.8C410.7 377.1 364.3 352 312 352c-88.4 0-160 71.6-160 160s71.6 160 160 160c52.3 0 98.7-25.1 127.9-63.8l196.8 142.5c-3.1 10.6-4.7 21.8-4.7 33.3 0 66.2 53.8 120 120 120s120-53.8 120-120-53.8-120-120-120zm0-476c28.7 0 52 23.3 52 52s-23.3 52-52 52-52-23.3-52-52 23.3-52 52-52zM312 600c-48.5 0-88-39.5-88-88s39.5-88 88-88 88 39.5 88 88-39.5 88-88 88zm440 236c-28.7 0-52-23.3-52-52s23.3-52 52-52 52 23.3 52 52-23.3 52-52 52z"></path>
-        </svg>
+    <a-popover placement="left">
+      <template #content>
+        <a-flex vertical align="center">
+          <a-qrcode ref="qrcodeCanvasRef" :value="`http://192.168.0.151:3002/post/${articleInfo.id}`"/>
+          <a-flex gap="8" justify="flex-end" align="center">
+            <span>分享二维码</span>
+            <span style="color: #1e80ff;cursor: pointer" @click="downloadChange">下载</span>
+          </a-flex>
+        </a-flex>
       </template>
-    </a-float-button>
+      <a-float-button :style="{marginTop: '25px'}">
+        <template #icon>
+          <svg viewBox="64 64 896 896" focusable="false" data-icon="share-alt" width="1em"
+               height="1em" fill="#8a919f" aria-hidden="true">
+            <path
+                d="M752 664c-28.5 0-54.8 10-75.4 26.7L469.4 540.8a160.68 160.68 0 000-57.6l207.2-149.9C697.2 350 723.5 360 752 360c66.2 0 120-53.8 120-120s-53.8-120-120-120-120 53.8-120 120c0 11.6 1.6 22.7 4.7 33.3L439.9 415.8C410.7 377.1 364.3 352 312 352c-88.4 0-160 71.6-160 160s71.6 160 160 160c52.3 0 98.7-25.1 127.9-63.8l196.8 142.5c-3.1 10.6-4.7 21.8-4.7 33.3 0 66.2 53.8 120 120 120s120-53.8 120-120-53.8-120-120-120zm0-476c28.7 0 52 23.3 52 52s-23.3 52-52 52-52-23.3-52-52 23.3-52 52-52zM312 600c-48.5 0-88-39.5-88-88s39.5-88 88-88 88 39.5 88 88-39.5 88-88 88zm440 236c-28.7 0-52-23.3-52-52s23.3-52 52-52 52 23.3 52 52-23.3 52-52 52z"></path>
+          </svg>
+        </template>
+      </a-float-button>
+    </a-popover>
   </a-float-button-group>
 
   <a-float-button-group shape="circle">
