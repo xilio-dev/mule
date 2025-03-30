99f97aa4d338f8786f05c1148dd65ea912d52e8b
xilio <termitetech@163.com>
2025-03-30 22:31:59 +0800

    feat(公告): 新增查询所有公告列表、获取公告详情、按类型获取接口

--- application/src/main/java/com/stackoak/stackoak/application/service/announcement/AnnouncementServiceImpl.java
+++ application/src/main/java/com/stackoak/stackoak/application/service/announcement/AnnouncementServiceImpl.java
@@ -1,14 +1,20 @@
 package com.stackoak.stackoak.application.service.announcement;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.stackoak.stackoak.application.exception.BizException;
+import com.stackoak.stackoak.common.data.CommonPageQuery;
+import com.stackoak.stackoak.common.data.PageQuery;
 import com.stackoak.stackoak.common.data.announcement.Announcement;
+import com.stackoak.stackoak.common.data.announcement.AnnouncementType;
 import com.stackoak.stackoak.repository.announcement.AnnouncementMapper;
 import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author stackoak.com
@@ -17,4 +23,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class AnnouncementServiceImpl extends ServiceImpl<AnnouncementMapper, Announcement> implements IAnnouncementService {
 
+    @Override
+    public Page<Announcement> getAnnouncements(PageQuery pageQuery) {
+        Page<Announcement> page = Page.of(pageQuery.getCurrent(), pageQuery.getSize());
+        LambdaQueryWrapper<Announcement> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Announcement::getIsPublished, true);
+        return page(page, wrapper);
+    }
+
+    @Override
+    public Page<Announcement> getAnnouncementsByType(CommonPageQuery pageQuery) {
+        Integer type = pageQuery.getType();
+        Page<Announcement> page = Page.of(pageQuery.getCurrent(), pageQuery.getSize());
+        AnnouncementType announcementType = AnnouncementType.fromCode(type);
+        BizException.noNull(announcementType, "必须指定正确的通知类型！");
+        LambdaQueryWrapper<Announcement> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Announcement::getIsPublished, true);
+        wrapper.eq(Announcement::getType, announcementType.getCode());
+        return page(page, wrapper);
+    }
+
+    @Override
+    public Announcement getAnnouncementById(String id) {
+        LambdaQueryWrapper<Announcement> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Announcement::getId, id);
+        wrapper.eq(Announcement::getIsPublished, true);
+        return getOne(wrapper);
+    }
 }
--- application/src/main/java/com/stackoak/stackoak/application/service/announcement/IAnnouncementService.java
+++ application/src/main/java/com/stackoak/stackoak/application/service/announcement/IAnnouncementService.java
@@ -9,17 +9,34 @@ import com.stackoak.stackoak.common.data.announcement.Announcement;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author stackoak.com
  * @since 2025-03-30 22:06:08
  */
 public interface IAnnouncementService extends IService<Announcement> {
-
+    /**
+     * 查询所有的公告
+     *
+     * @param pageQuery 分页参数
+     * @return 所有已发布公告列表
+     */
     Page<Announcement> getAnnouncements(PageQuery pageQuery);
 
+    /**
+     * 通过类型获取已发布公告列表 【已发布】
+     *
+     * @param pageQuery 分页查询
+     * @return 公告列表
+     */
     Page<Announcement> getAnnouncementsByType(CommonPageQuery pageQuery);
 
+    /**
+     * 获取已发布公告详情
+     *
+     * @param id 公告id
+     * @return 详情
+     */
     Announcement getAnnouncementById(String id);
 }
